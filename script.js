f15e3a12c3f0e3c8606095d7d4ccdd4703e603c8
Sandip Chhetry <sandyc4136@gmail.com>
2023-02-17 02:28:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+var btn=document.getElementById('addTodoBtn');
+var ol=document.getElementById('todoList');
+var input=document.getElementById('newTodoInput');
+
+function todofunction(){
+	if(input.value){
+		var li=document.createElement('li');
+		li.textContent=input.value;
+		ol.appendChild(li);
+		input.value="";
+	}
+}
+btn.addEventListener("click",todofunction);
 
